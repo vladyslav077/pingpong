dec148b93dd31e93de5fd2b1d4527b0628a57428
lpopivnyak <stadnykroman2010@gmail.com>
2024-02-11 18:25:23 +0200

    dfd34

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
+import requests
+import time
 
+def get_weather(city):
+    api_key = 'f81703c1f3b81ad93e6644153c4a426e'  # Отримайте свій API ключ на сайті OpenWeatherMap
+    url = f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}'
+    response = requests.get(url)
+    data = response.json()
+    if data['cod'] == 200:
+        weather_info = data['weather'][0]['description']
+        temp = data['main']['temp']
+        print(f"Погода у місті {city}: {weather_info}, температура: {temp}°C")
+    else:
+        print("Не вдалося отримати дані про погоду.")
+
+cities = ['Київ', 'Львів', 'Одеса', 'Харків']  # Додайте інші міста України, які вам цікаві
+
+while True:
+    for city in cities:
+        get_weather(city)
+    time.sleep(300)  # Оновлення погоди кожні 5 хвилин
